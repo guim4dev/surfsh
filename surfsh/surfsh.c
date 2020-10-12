59df373fbe4a9f9f1d5af4492519f8cf9180cedf
Thiago Guimaraes <thiagoguimaraes.dev@gmail.com>
2020-10-12 18:58:41 -0300

    It works! Adding better color scheme also

--- surfsh/surfsh.c
+++ surfsh/surfsh.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+#include <string.h>
 #include <sys/types.h>
 #include <unistd.h>
 #include <sys/wait.h>
-#define MAX_LEN 20
+#define BASE_LEN 50
 
 void init_shell() {
 		printf("\033[1;32m");
@@ -14,49 +15,56 @@ void init_shell() {
         "***********************");
     printf("\n\n");
     sleep(1);
-		printf("\033[1;36m");
 }
 
 void getUserStringInput(char *str, char *reference) {
-	printf("\n%s\n", str);
+	printf("\033[1;36m");
+	printf("%s\n", str);
+	printf("\033[1;32m");
 	scanf("%s", reference);
+	printf("\033[0m");
+	printf("\n");
 }
 
 void getUserIntInput(char *str, int *reference) {
-	printf("\n%s\n", str);
+	printf("\033[1;36m");
+	printf("%s\n", str);
+	printf("\033[1;32m");
 	scanf("%i", reference);
+	printf("\033[0m");
+	printf("\n");
 }
 
 int main(void){
 	init_shell();
 
-	char path[MAX_LEN] = "/bin/";
-	char command[MAX_LEN];
+	char path[2*BASE_LEN] = "/bin/";
+	char command[BASE_LEN];
+	char argument[BASE_LEN];
 	int numberOfArguments = 0;
 
 	getUserStringInput("Qual comando quer executar?", command);
+	strcat(path, command);
 	getUserIntInput("Quantos argumentos você quer digitar?", &numberOfArguments);
-	char arguments[numberOfArguments+1];
+	char* arguments[numberOfArguments+2];
 	arguments[0] = command;
+	arguments[numberOfArguments+1] = NULL;
 
 	int i;
 	for (i = 1; i <= numberOfArguments; ++i) {
-		char argument[MAX_LEN];
 		getUserStringInput("Insira o argumento:", argument);
 		arguments[i] = argument;
 	}
-	printf("%s\n", command);
-	printf("%s\n", path);
-	printf("%i\n", numberOfArguments);
-	// pid_t child_pid = fork();
-	// if (child_pid == 0) {
-	// 	//Run ping command
-	// 	execlp("/bin/ping","ping","8.8.8.8","-c","50",NULL);
-	// 	return 0;
-	// }
-	// else {
-	// 	wait(NULL);
-	// 	printf("Task is done\n");
-	// 	return 0;
-	// }
+
+	pid_t child_pid = fork();
+	if (child_pid == 0) {
+		execv(path, arguments);
+		return 0;
+	}
+	else {
+		wait(NULL);
+		printf("\033[1;32m");
+		printf("\nTarefa concluída.\n");
+		return 0;
+	}
 }
